70580bdaa45ed6390fef4b12bb1a3165d7275c89
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-03-26 14:31:38 -0600

    Saving the user value in Local Storage.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import AuthenticationCard from './components/Authentication/AuthenticationCard';
 import Header from './components/Header/Header';
 import WelcomeCard from './components/Welcome/WelcomeCard';
 
 function App() {
-  const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(undefined);
+
+  useEffect(() => {
+    const response = localStorage.getItem('user');
+    setIsLogin(response);
+  }, []);
 
   const handleLogin = (account) => {
     setIsLogin(true);
+    localStorage.setItem('user', true);
   }
 
   const handleLogout = () => {
+    localStorage.removeItem('user', false);
     setIsLogin(false);
   }
 
